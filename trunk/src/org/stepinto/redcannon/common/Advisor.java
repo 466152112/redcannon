4e72280ad17df51784de78741b3f59ebbf1dbedd
CharlesCPP <CharlesCPP@a07deae6-f522-11dd-802b-177316e4ac06>
2009-02-08 09:58:22 +0000

    more rules & units

--- trunk/src/org/stepinto/redcannon/common/Advisor.java
+++ trunk/src/org/stepinto/redcannon/common/Advisor.java
@@ -23,33 +23,12 @@ public class Advisor extends Unit {
 			int nx = x + dx;
 			int ny = y + dy;
 			
-			if (Position.isValid(nx, ny) && board.getColorAt(nx, ny) != player && isInPalace(nx, ny)) {
+			if (Position.isValid(nx, ny) && board.getColorAt(nx, ny) != player && GameUtility.isInPalace(player, nx, ny)) {
 				legalMoves[count] = new Position(x, y);
 				count++;
 			}
 		}
 		
 		return Arrays.copyOf(legalMoves, count);
-	}
-
-	private boolean isInPalace(int x, int y) {
-		final int PALACE_MIN_X = 3;
-		final int PALACE_MAX_X = 5;
-		int player = getColor();
-		
-		if (PALACE_MIN_X <= x && x <= PALACE_MAX_X) {
-			if (GameUtility.getAttackDirection(player) == 1) {
-				final int PALACE_MIN_Y = 0;
-				final int PALACE_MAX_Y = 3;
-				return PALACE_MIN_Y <= y && y <= PALACE_MAX_Y;
-			}
-			else {
-				final int PALACE_MIN_Y = 7;
-				final int PALACE_MAX_Y = 9;
-				return PALACE_MIN_Y <= y && y <= PALACE_MAX_Y;
-			}		
-		}
-		else
-			return false;
 	}
 }
---
+++ trunk/src/org/stepinto/redcannon/common/General.java
@@ -0,0 +1,43 @@
+package org.stepinto.redcannon.common;
+
+import java.util.*;
+
+public class General extends Unit {
+	public General(int color, Position position) {
+		super(color, position);
+	}
+
+	@Override
+	public Position[] getLegalMoves(BoardImage board) {
+		final int DX[] = {1, -1, 0, 0};
+		final int DY[] = {0, 0, 1, -1};
+		int player = getColor();
+		Position[] legalMoves = new Position[5];
+		int count = 0;
+		
+		for (int i = 0; i < DX.length; i++) {
+			int x = getPosition().getX() + DX[i];
+			int y = getPosition().getY() + DY[i];
+			if (Position.isValid(x, y) && GameUtility.isInPalace(player, x, y) && board.getColorAt(x, y) != player) {
+				legalMoves[count] = new Position(x, y);
+				count++;
+			}
+		}
+		
+		// flying move
+		int attackDir = GameUtility.getAttackDirection(player);
+		int x = getPosition().getX();
+		int y = getPosition().getY() + attackDir;
+		while (Position.isValid(x, y) && board.isEmptyAt(x, y))
+			y += attackDir;
+		if (Position.isValid(x, y)) {
+			Unit unit = board.getUnitAt(x, y);
+			if (unit instanceof General) {
+				legalMoves[count] = new Position(x, y);
+				count++;
+			}
+		}
+		
+		return Arrays.copyOf(legalMoves, count);
+	}
+}
