ab6bd21d749b983cebfc20d1caaeea956a20cd8c
CharlesCPP <CharlesCPP@a07deae6-f522-11dd-802b-177316e4ac06>
2009-02-24 15:15:51 +0000

    server mode jvm

--- trunk/run_debugger.sh
+++ trunk/run_debugger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source set_classpath.sh
-java -Xmx1024m -cp $CLASSPATH org.stepinto.redcannon.ai.log.SearchDebugger $*
+java -server -Xmx1024m -cp $CLASSPATH org.stepinto.redcannon.ai.log.SearchDebugger $*
 
