0b33c0b2783cbd26891dae19719beea595b29e6f
CharlesCPP <CharlesCPP@a07deae6-f522-11dd-802b-177316e4ac06>
2009-03-05 13:40:39 +0000

    script update

--- set_classpath.sh
+++ set_classpath.sh
@@ -3,12 +3,14 @@
 uname | grep -iq cygwin
 if [ $? -eq 0 ]
 then
+	swt=swt-win32.jar
 	sep=';'
 else
+	swt=swt-gtk.jar
 	sep=':'
 fi
 
-CLASSPATH=bin
+CLASSPATH=bin${sep}lib/$swt
 for f in lib/*.jar
 do
 	CLASSPATH+=$sep$f
